e6843f0ba00b5ce1f208038b0265797ddd5d6544
DEVLOG.E09 <DEVLOG.E09@afpar.com>
2020-10-21 14:44:12 +0400

    v3.1

--- controller/controleur.php
+++ controller/controleur.php
@@ -17,10 +17,10 @@
             $view = "403";
         }
     }
-    
+
     /* Verifie que le prix est supérieur à 0 */
     function verifPrix() {
-        if (intval($_POST['prix']) > 0) {
+        if (intval($_POST['prix']) > 0 && intval($_POST['prix']) < 99999999999.99) {
             return true;    
         } else {
             return false;
