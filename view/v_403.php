e619dd3357c618cb047723eaa3883b139630d017
RomainLefranc <lefranc46@gmail.com>
2021-05-16 22:33:14 +0400

    suppression fichier inutile

--- view/v_403.php
+++ view/v_403.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Erreur</title>
         <!-- Bootstrap core CSS -->
-        <link href="assets/bootstrap/css/bootstrap.css" rel="stylesheet">
+        <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <!-- CSS Perso -->
         <link rel="stylesheet" href="css/style.css">
     </head>
--- view/v_accueil.php
+++ view/v_accueil.php
@@ -5,7 +5,7 @@
       <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
       <!--   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">-->
       <title>Surf Wave</title>
-      <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css">
+      <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
       <link rel="stylesheet" href="css/style.css">
    </head>
    <body>
@@ -40,7 +40,7 @@
          /* Footer */
          include "accueil/accueil_footer.php";
       ?>
-      <script src="assets/jquery/jquery.js"></script>
+      <script src="assets/jquery/jquery.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.js" integrity="sha512-JGKswjfABjJjtSUnz+y8XUBjBlwM1UHNlm2ZJN7A2a9HUYT3Mskq+SacsI35k4lok+/zetSxhZjKS3r3tfAnQg==" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
